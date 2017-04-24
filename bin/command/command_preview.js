c194c6ae8e7693f76101b112832416f36e1f9715
Fábio Rogério <fabio.rogerio@tecnospeed.com.br>
2017-04-24 11:41:01 -0300

    add feature preview

---
+++ bin/command/command_preview.js
@@ -0,0 +1,56 @@
+var fs = require('fs');
+var project = require('../utils/project');
+var component = require('../utils/component');
+var finalhandler = require('finalhandler');
+var http = require('http');
+var serveStatic = require('serve-static');
+
+var DEFAULT_HTTP_PORT = 8080;
+var serve = serveStatic('www', {'index': ['index.html', 'index.htm']});
+var mockCordvaJS = ''
+mockCordvaJS += 'var socket = io(location.origin);'
+mockCordvaJS += 'socket.on("reload", function (data) { ';
+mockCordvaJS += '   location.reload(); ';
+mockCordvaJS += '})'
+
+module.exports = {
+  run : function (){
+    var self = this;
+    if(commands.port) {
+      DEFAULT_HTTP_PORT = commands.port
+    }
+    var server = http.createServer(function onRequest (req, res) {
+      //console.log(req.url)
+      if(req.url === '/cordova.js'){
+        res.write(mockCordvaJS);
+        res.end();
+      } else if(req.url === '/favicon.ico'){
+        res.write('');
+        res.end();
+      } else if(req.url === '/' || req.url === '/index.html'){
+        var done = finalhandler(req, res)
+        fs.readFile('./www/index.html', function (err, buf) {
+          if (err) return done(err)
+          res.setHeader('Content-Type', 'text/html')
+          var html = buf.toString();
+          html = html.replace('</head>','<script src="/socket.io/socket.io.js"></script></head>');
+          res.end(html)
+        })
+      } else {
+        serve(req, res, finalhandler(req, res))
+      }
+    });
+    server.listen(DEFAULT_HTTP_PORT, function(a){
+      console.log(" RUNNING: ".bgGreen,"Server for preview app running on port "+DEFAULT_HTTP_PORT);
+      console.log("For stop this server hit Ctrl+C.".grey)
+    });
+
+    var io = require('socket.io')(server);
+    io.on('connection', function (socket) {
+
+    });
+    require('chokidar').watch('./www', {ignored: /[\/\\]\./}).on('all', function(event, path) {
+      io.sockets.emit('reload', { event: event, path:path });
+    });
+  }
+}
