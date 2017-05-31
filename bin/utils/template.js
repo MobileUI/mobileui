23c35016b5ac4d6086d6a1c1036a60cd459f3764
Fábio Rogério <fabio.rogerio@tecnospeed.com.br>
2017-05-31 15:49:15 -0300

    add template download

---
+++ bin/utils/template.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+var request = require('request');
+
+module.exports = {
+
+  install: function(templateName, callback){
+    var self = this;
+    var folder = ''
+    if(fs.existsSync("./www")) {
+      folder = "/www"
+    }
+    var headerRequest = { uri: repoTemplates+templateName+'/template.json', rejectUnauthorized: false }
+    request(headerRequest, function (error, response, body) {
+      if(response && response.statusCode === 200) {
+        var temp = JSON.parse(body);
+        if(temp.files && temp.files.length){
+          var totalFiles = temp.files.length;
+          var filesDownloaded = 0;
+          var download = function(){
+            if(temp.files[filesDownloaded].split('/').length > 1){
+              var folderDown = temp.files[filesDownloaded].split('/')[0]
+              if (!fs.existsSync("."+folder+"/"+folderDown)){
+                  fs.mkdirSync("."+folder+"/"+folderDown);
+              }
+            }
+            var msgDown = "> File "+temp.files[filesDownloaded]+" downloaded";
+            console.log(msgDown.grey)
+            var headerRequest = { uri: repoTemplates+templateName+'/'+temp.files[filesDownloaded], rejectUnauthorized: false }
+            var req = request(headerRequest)
+            .pipe(fs.createWriteStream("."+folder+"/"+temp.files[filesDownloaded]))
+            .on('close', function (err) {
+              filesDownloaded++;
+              if(totalFiles === filesDownloaded) {
+                callback();
+              } else {
+                download();
+              }
+            })
+          }
+          download();
+        } else {
+          console.log(" ERROR: ".bgRed, "Sorry, this template not ok for now.")
+        }
+      }
+    });
+  }
+
+}
