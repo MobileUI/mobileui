3b8761b9d69a2ee411aa19c0708c4a77cfe3be33
Fábio Rogério SJ <fabio.rogerio.sj@gmail.com>
2017-06-04 18:12:24 -0300

    add mock

---
+++ mock/cordova.js
@@ -0,0 +1,25 @@
+var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+var systemDevice = "unknown";
+
+if (/windows phone/i.test(userAgent)) {
+    systemDevice = "Windows Phone";
+}
+
+if (/android/i.test(userAgent)) {
+    systemDevice = "Android";
+}
+
+if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    systemDevice = "iOS";
+}
+
+window.device = {
+  cordova : "6.0.1",
+  model : "M001",
+  platform : systemDevice,
+  uuid : "dd00dd00dd00",
+  version : "6.0.1",
+  manufacturer : "M002",
+  isVirtual : false,
+  serial : "00001"
+}
