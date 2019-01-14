aeb07b8f99c2e3ccde1ad77bf6ac108124c3832f
Fábio Rogério <fabio@tindin.com.br>
2019-01-14 21:44:19 -0200

    remove /r

--- bin/command/command_install.js
+++ bin/command/command_install.js
@@ -1,146 +1,144 @@
-var project = require('../utils/project');
-var component = require('../utils/component');
-var getfont = require('../utils/getfont');
-var template = require('../utils/template');
-var environment = require('../utils/environment');
-var request = require('request');
+var project = require('../utils/project')
+var component = require('../utils/component')
+var getfont = require('../utils/getfont')
+var template = require('../utils/template')
+var environment = require('../utils/environment')
+var request = require('request')
 
 module.exports = {
-    run : function (){
-      var self = this;
-      // if(!project.checkIntoProject()) {
-      //   return console.log(" ERROR: ".bgRed, "You are not in the folder of a Cordova project.")
-      // }
-      if(commands._.length < 2) {
-        return console.log(" ERROR: ".bgRed, "You must enter the name of the component.")
-      }
-      var indexComponent = 1;
-      var componentName = commands._[indexComponent]
-
-      var installComponents = function(){
-        if(componentName === 'font') {
-          if(commands._.length !== 3){
-            console.log(" ERROR: ".bgRed, "To install a font you need to pass the font name after the font command.")
-            console.log("If you need help see de doc: https://mobileui.github.io/#fonts".grey)
-            return false
-          }
-          var fontName = commands._[2]
-          getfont.install(fontName)
-        } else if(componentName === 'template') {
-          if(commands._.length !== 3){
-            console.log(" ERROR: ".bgRed, "To install a template you need to pass the name of template after the template command.")
-            console.log("If you need help see de doc: https://mobileui.github.io/#templates".grey)
-            return false
-          }
-          var templateName = commands._[2]
-          template.install(templateName, function(){
-            console.log(" SUCCESS: ".bgGreen,"Template "+templateName+" installed success!")
-          })
-        } else if(componentName === 'env') {
-          if(commands._.length !== 3){
-            console.log(" ERROR: ".bgRed, "To install a environment you need to pass the system build (android, ios, wp).")
-            console.log("If you need help see de doc: https://mobileui.github.io/#environment".grey)
-            return false
-          }
-          var envName = commands._[2]
-          environment.install(envName, function(res){
-            
-          })
-        } else {
-          self.install(componentName, function(){
-            indexComponent++;
-            componentName = commands._[indexComponent]
-            if(componentName){
-              installComponents();
-            }
-          })
-        }
-      }
-
-      installComponents();
-
-    },
-    install: function(componentName, callback){
-      var self = this;
-      var headerRequest = { uri: repoComponents+componentName+'.json', rejectUnauthorized: false }
-      request(headerRequest, function (error, response, body) {
-        if(response && response.statusCode === 200) {
-          var componentJson = JSON.parse(body);
-          var installedMessage = component.checkInstalled(componentName)
-          if (!commands.update && installedMessage) {
-            console.log(" EXIST: ".bgBlue,"The component ",componentName," has already been installed!")
-            console.log("If you need reinstall put --update in command.".grey)
-            callback()
-            return false;
-          }
-          component.install(componentJson, function(err){
-            if(err){
-              console.log(" ERROR: ".bgRed, "Sorry, the component could not be installed at this time.\n",err)
-              callback()
-              return false;
-            }
-            if(installedMessage) {
-              console.log(" SUCCESS: ".bgYellow,"Component "+componentName+" updated success!")
-            } else {
-              console.log(" SUCCESS: ".bgGreen,"Component "+componentName+" installed success!")
-            }
-            if(componentJson.dependencies && componentJson.dependencies.length){
-              console.log("> Installing dependent components...".grey)
-              var totalDependencies = componentJson.dependencies.length;
-              var totalDownloaded = 0;
-              var installDependency = function(){
-                var compInstallDepName = componentJson.dependencies[totalDownloaded]
-                var installedMessage = component.checkInstalled(compInstallDepName)
-                if (!commands.update && installedMessage) {
-                  var msg = "> The component "+compInstallDepName+" has already been installed!"
-                  console.log(msg.grey)
-                  totalDownloaded++;
-                  if(totalDependencies === totalDownloaded) {
-                      callback()
-                  } else {
-                    installDependency();
-                  }
-                } else {
-                  var headerRequest = { uri: repoComponents+compInstallDepName+'.json', rejectUnauthorized: false }
-                  request(headerRequest, function (error, response, body) {
-                    if(response && response.statusCode === 200) {
-                      var componentJsonDep = JSON.parse(body);
-                      component.install(componentJsonDep, function(err){
-                        if(err) return console.log(" ERROR: ".bgRed, "Sorry, the component could not be installed at this time.\n",err)
-                        if(installedMessage) {
-                          console.log(" SUCCESS: ".bgYellow,"Component "+ compInstallDepName+" updated success!")
-                        } else {
-                          console.log(" SUCCESS: ".bgGreen,"Component "+ compInstallDepName+" installed success!")
-                        }
-                        totalDownloaded++;
-                        if(totalDependencies === totalDownloaded) {
-                            callback()
-                        } else {
-                          installDependency();
-                        }
-                      });
-                    } else {
-                      console.log(" ERROR: ".bgRed, "Component "+ componentJson.dependencies[totalDownloaded]+" not exist.")
-                      totalDownloaded++;
-                      if(totalDependencies === totalDownloaded) {
-                          callback()
-                      } else {
-                        installDependency();
-                      }
-                    }
-                  });
-                }
-              }
-              installDependency();
-            } else {
-              callback();
-            }
-          })
-
-        } else {
-          console.log(" ERROR: ".bgRed, "Component "+componentName+" not exist.")
-          callback()
-        }
-      });
+  run: function () {
+    var self = this
+    // if(!project.checkIntoProject()) {
+    //   return console.log(" ERROR: ".bgRed, "You are not in the folder of a Cordova project.")
+    // }
+    if (commands._.length < 2) {
+      return console.log(' ERROR: '.bgRed, 'You must enter the name of the component.')
     }
+    var indexComponent = 1
+    var componentName = commands._[indexComponent]
+
+    var installComponents = function () {
+      if (componentName === 'font') {
+        if (commands._.length !== 3) {
+          console.log(' ERROR: '.bgRed, 'To install a font you need to pass the font name after the font command.')
+          console.log('If you need help see de doc: https://mobileui.github.io/#fonts'.grey)
+          return false
+        }
+        var fontName = commands._[2]
+        getfont.install(fontName)
+      } else if (componentName === 'template') {
+        if (commands._.length !== 3) {
+          console.log(' ERROR: '.bgRed, 'To install a template you need to pass the name of template after the template command.')
+          console.log('If you need help see de doc: https://mobileui.github.io/#templates'.grey)
+          return false
+        }
+        var templateName = commands._[2]
+        template.install(templateName, function () {
+          console.log(' SUCCESS: '.bgGreen, 'Template ' + templateName + ' installed success!')
+        })
+      } else if (componentName === 'env') {
+        if (commands._.length !== 3) {
+          console.log(' ERROR: '.bgRed, 'To install a environment you need to pass the system build (android, ios, wp).')
+          console.log('If you need help see de doc: https://mobileui.github.io/#environment'.grey)
+          return false
+        }
+        var envName = commands._[2]
+        environment.install(envName, function (res) {
+
+        })
+      } else {
+        self.install(componentName, function () {
+          indexComponent++
+          componentName = commands._[indexComponent]
+          if (componentName) {
+            installComponents()
+          }
+        })
+      }
+    }
+
+    installComponents()
+  },
+  install: function (componentName, callback) {
+    var self = this
+    var headerRequest = { uri: repoComponents + componentName + '.json', rejectUnauthorized: false }
+    request(headerRequest, function (error, response, body) {
+      if (response && response.statusCode === 200) {
+        var componentJson = JSON.parse(body)
+        var installedMessage = component.checkInstalled(componentName)
+        if (!commands.update && installedMessage) {
+          console.log(' EXIST: '.bgBlue, 'The component ', componentName, ' has already been installed!')
+          console.log('If you need reinstall put --update in command.'.grey)
+          callback()
+          return false
+        }
+        component.install(componentJson, function (err) {
+          if (err) {
+            console.log(' ERROR: '.bgRed, 'Sorry, the component could not be installed at this time.\n', err)
+            callback()
+            return false
+          }
+          if (installedMessage) {
+            console.log(' SUCCESS: '.bgYellow, 'Component ' + componentName + ' updated success!')
+          } else {
+            console.log(' SUCCESS: '.bgGreen, 'Component ' + componentName + ' installed success!')
+          }
+          if (componentJson.dependencies && componentJson.dependencies.length) {
+            console.log('> Installing dependent components...'.grey)
+            var totalDependencies = componentJson.dependencies.length
+            var totalDownloaded = 0
+            var installDependency = function () {
+              var compInstallDepName = componentJson.dependencies[totalDownloaded]
+              var installedMessage = component.checkInstalled(compInstallDepName)
+              if (!commands.update && installedMessage) {
+                var msg = '> The component ' + compInstallDepName + ' has already been installed!'
+                console.log(msg.grey)
+                totalDownloaded++
+                if (totalDependencies === totalDownloaded) {
+                  callback()
+                } else {
+                  installDependency()
+                }
+              } else {
+                var headerRequest = { uri: repoComponents + compInstallDepName + '.json', rejectUnauthorized: false }
+                request(headerRequest, function (error, response, body) {
+                  if (response && response.statusCode === 200) {
+                    var componentJsonDep = JSON.parse(body)
+                    component.install(componentJsonDep, function (err) {
+                      if (err) return console.log(' ERROR: '.bgRed, 'Sorry, the component could not be installed at this time.\n', err)
+                      if (installedMessage) {
+                        console.log(' SUCCESS: '.bgYellow, 'Component ' + compInstallDepName + ' updated success!')
+                      } else {
+                        console.log(' SUCCESS: '.bgGreen, 'Component ' + compInstallDepName + ' installed success!')
+                      }
+                      totalDownloaded++
+                      if (totalDependencies === totalDownloaded) {
+                        callback()
+                      } else {
+                        installDependency()
+                      }
+                    })
+                  } else {
+                    console.log(' ERROR: '.bgRed, 'Component ' + componentJson.dependencies[totalDownloaded] + ' not exist.')
+                    totalDownloaded++
+                    if (totalDependencies === totalDownloaded) {
+                      callback()
+                    } else {
+                      installDependency()
+                    }
+                  }
+                })
+              }
+            }
+            installDependency()
+          } else {
+            callback()
+          }
+        })
+      } else {
+        console.log(' ERROR: '.bgRed, 'Component ' + componentName + ' not exist.')
+        callback()
+      }
+    })
+  }
 }
--- bin/command/command_preview.js
+++ bin/command/command_preview.js
@@ -1,60 +1,60 @@
-var fs = require('fs');
-var project = require('../utils/project');
-var component = require('../utils/component');
-var finalhandler = require('finalhandler');
-var http = require('http');
-var serveStatic = require('serve-static');
+var fs = require('fs')
+var project = require('../utils/project')
+var component = require('../utils/component')
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
 
-var DEFAULT_HTTP_PORT = 8080;
+var DEFAULT_HTTP_PORT = 8080
 var folder = ''
-if(fs.existsSync("./www")) {
-  folder = "www/"
+if (fs.existsSync('./www')) {
+  folder = 'www/'
 }
-var serve = serveStatic('./'+folder, {'index': ['index.html', 'index.htm']});
+var serve = serveStatic('./' + folder, {'index': ['index.html', 'index.htm']})
 var mockCordvaJS = ''
 mockCordvaJS += 'var socket = io(location.origin);'
-mockCordvaJS += 'socket.on("reload", function (data) { ';
-mockCordvaJS += '   location.reload(); ';
+mockCordvaJS += 'socket.on("reload", function (data) { '
+mockCordvaJS += '   location.reload(); '
 mockCordvaJS += '})'
 
 module.exports = {
-  run : function (){
-    var self = this;
-    if(commands.port) {
+  run: function () {
+    var self = this
+    if (commands.port) {
       DEFAULT_HTTP_PORT = commands.port
     }
     var server = http.createServer(function onRequest (req, res) {
-      //console.log(req.url)
-      if(req.url === '/cordova.js'){
-        res.write(mockCordvaJS);
-        res.end();
-      } else if(req.url === '/favicon.ico'){
-        res.write('');
-        res.end();
-      } else if(req.url === '/' || req.url === '/index.html'){
+      // console.log(req.url)
+      if (req.url === '/cordova.js') {
+        res.write(mockCordvaJS)
+        res.end()
+      } else if (req.url === '/favicon.ico') {
+        res.write('')
+        res.end()
+      } else if (req.url === '/' || req.url === '/index.html') {
         var done = finalhandler(req, res)
-        fs.readFile('./'+folder+'index.html', function (err, buf) {
+        fs.readFile('./' + folder + 'index.html', function (err, buf) {
           if (err) return done(err)
           res.setHeader('Content-Type', 'text/html')
-          var html = buf.toString();
-          html = html.replace('</head>','<script src="/socket.io/socket.io.js"></script><script  type="text/javascript" src="https://rawgit.com/MobileUI/mobileui/master/mock/cordova.js"></script></head>');
+          var html = buf.toString()
+          html = html.replace('</head>', '<script src="/socket.io/socket.io.js"></script><script  type="text/javascript" src="https://rawgit.com/MobileUI/mobileui/master/mock/cordova.js"></script></head>')
           res.end(html)
         })
       } else {
         serve(req, res, finalhandler(req, res))
       }
-    });
-    server.listen(DEFAULT_HTTP_PORT, function(a){
-      console.log(" RUNNING: ".bgGreen,"Server for preview app running on http://localhost:"+DEFAULT_HTTP_PORT);
-      console.log("For stop this server hit Ctrl+C.".grey)
-    });
+    })
+    server.listen(DEFAULT_HTTP_PORT, function (a) {
+      console.log(' RUNNING: '.bgGreen, 'Server for preview app running on http://localhost:' + DEFAULT_HTTP_PORT)
+      console.log('For stop this server hit Ctrl+C.'.grey)
+    })
 
-    var io = require('socket.io')(server);
+    var io = require('socket.io')(server)
     io.on('connection', function (socket) {
 
-    });
-    require('chokidar').watch('./'+folder, {ignored: /[\/\\]\./}).on('all', function(event, path) {
-      io.sockets.emit('reload', { event: event, path:path });
-    });
+    })
+    require('chokidar').watch('./' + folder, {ignored: /[\/\\]\./}).on('all', function (event, path) {
+      io.sockets.emit('reload', { event: event, path: path })
+    })
   }
 }
